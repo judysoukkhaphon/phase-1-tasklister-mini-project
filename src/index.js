a0b372077af46e8ec7a4384dca93a97e18bc1ba6
Judy Soukkhaphon <judysoukkhaphon@gmail.com>
2023-01-21 15:12:54 -0500

    can only enter one task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+
+
+
+var submit = document.getElementById('submit');
+
+submit.addEventListener('click', eventfunction);
+
+
+
+function eventfunction(event) {
+  var newTask = document.getElementById('new-task-description').value;
+  let taskList = document.getElementById('tasks');
+  taskList.innerHTML = newTask;
+  event.preventDefault();
+}
